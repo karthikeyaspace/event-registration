c819b4355c0abdaa68e87b5ebdcd993e035bc14a
Parshi Praneesh <parshipraneesh8@gmail.com>
2024-04-25 16:29:08 +0000

    Co-authored-by: Karthikeya <karthikeyaspace@users.noreply.github.com>

---
+++ client/src/Root.jsx
@@ -0,0 +1,12 @@
+import PhonePay from "./components/PhonePay";
+import Failure from "./components/Failure";
+import Success from "./components/Success";
+import { Outlet } from "react-router-dom";
+
+export default function Root() {
+    return (
+        <div>
+        <Outlet />
+        </div>
+    );
+}
