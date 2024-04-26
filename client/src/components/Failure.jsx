566e8c2f081ded1bbf47d465ee67d69e2c4527bd
Parshi Praneesh <parshipraneesh8@gmail.com>
2024-04-26 07:17:56 +0000

    PORT in env

--- client/src/components/Failure.jsx
+++ client/src/components/Failure.jsx
@@ -1,8 +1,8 @@
 export default function Failure(){
     return (
         <div className="container">
-            <div className="failure ">
-                <h1 className="text-red text-3xl m-auto">Payment failed</h1>
+            <div className="failure">
+                <h1>Payment failed</h1>
             </div>
         </div>
     )
--- server/server.js
+++ server/server.js
@@ -28,6 +28,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 const phoneperoute = require("./routes/phoneperoute")
 app.use("/register", phoneperoute)
 
-app.listen(3000, () => {
+app.listen(process.env.PORT||3000, () => {
     console.log("Server is running on port 3000")
 })
